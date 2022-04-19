3828b8297103d4ceb21320334b891b074b0c039d
Giles Burdett <giles@cogniworks.co.uk>
2022-04-19 17:26:46 +0100

    TableOfContents ContentSection now uses Localisation struct for its Title

--- model/table_of_contents.go
+++ model/table_of_contents.go
@@ -1,10 +1,16 @@
 package model
 
+/* ContentSection maps the content details.
+The visible text can be either a 'Localisation.Text' or a 'Localisation.LocaleKey'.
+The 'LocaleKey' has to correspond to the localisation key found in the toml files within assets/locales, otherwise the page will error.
+Plural refers to the plural int used in the toml file.
+*/
 type ContentSection struct {
-	Current bool   `json:"current"`
-	Title   string `json:"title"`
+	Current bool         `json:"current"`
+	Title   Localisation `json:"title"`
 }
 
+// TableOfContents contains the contents of the page
 type TableOfContents struct {
 	AriaLabel    Localisation              `json:"aria_label"`
 	Title        Localisation              `json:"title"`
