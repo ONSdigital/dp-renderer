0f0a8cb3a4a8a08bba91785d16bd947f6ca52956
Giles Burdett <giles@cogniworks.co.uk>
2022-04-21 09:05:32 +0100

    Move localisation logic into model method

--- model/localisation.go
+++ model/localisation.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"github.com/ONSdigital/dp-renderer/helper"
+)
+
 /* Localisation offers text or a localised substitute.
 Text is displayed as-is.
 LocaleKey is a key into the toml files found in assets/locales.
@@ -9,4 +13,17 @@ type Localisation struct {
 	Text      string `json:"text"`
 	LocaleKey string `json:"locale_key"`
 	Plural    int    `json:"plural"`
+}
+
+// Localise by preference when a LocaleKey is provided, defaulting to Text otherwise.
+func (localisation Localisation) Localise(language string) string {
+	var result string
+
+	if localisation.LocaleKey != "" {
+		result = helper.Localise(localisation.LocaleKey, language, localisation.Plural)
+	} else {
+		result = localisation.Text
+	}
+
+	return result
 }
