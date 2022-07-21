1a363af4a8797fb76a82108f7d45914743734aec
Liam Lewis <liamjlewis@gmail.com>
2022-07-21 19:34:19 +0100

    added type NavigationItem to avoid getting a type from dp-topic-api

--- model/page.go
+++ model/page.go
@@ -1,40 +1,43 @@
 package model
 
-import (
-	topicModel "github.com/ONSdigital/dp-topic-api/models"
-)
-
 // Page contains data re-used for each page type a Data struct for data specific to the page type
 type Page struct {
-	Count                            int                    `json:"count"`
-	Type                             string                 `json:"type"`
-	DatasetId                        string                 `json:"dataset_id"`
-	DatasetTitle                     string                 `json:"dataset_title"`
-	URI                              string                 `json:"uri"`
-	Taxonomy                         []TaxonomyNode         `json:"taxonomy"`
-	Breadcrumb                       []TaxonomyNode         `json:"breadcrumb"`
-	IsInFilterBreadcrumb             bool                   `json:"is_in_filter_breadcrumb"`
-	ServiceMessage                   string                 `json:"service_message"`
-	Metadata                         Metadata               `json:"metadata"`
-	SearchDisabled                   bool                   `json:"search_disabled"`
-	SiteDomain                       string                 `json:"-"`
-	PatternLibraryAssetsPath         string                 `json:"-"`
-	Language                         string                 `json:"language"`
-	IncludeAssetsIntegrityAttributes bool                   `json:"-"`
-	ReleaseDate                      string                 `json:"release_date"`
-	BetaBannerEnabled                bool                   `json:"beta_banner_enabled"`
-	CookiesPreferencesSet            bool                   `json:"cookies_preferences_set"`
-	CookiesPolicy                    CookiesPolicy          `json:"cookies_policy"`
-	HasJSONLD                        bool                   `json:"has_jsonld"`
-	FeatureFlags                     FeatureFlags           `json:"feature_flags"`
-	Error                            Error                  `json:"error"`
-	EmergencyBanner                  EmergencyBanner        `json:"emergency_banner"`
-	Collapsible                      Collapsible            `json:"collapsible"`
-	Pagination                       Pagination             `json:"pagination"`
-	TableOfContents                  TableOfContents        `json:"table_of_contents"`
-	BackTo                           BackTo                 `json:"back_to"`
-	SearchNoIndexEnabled             bool                   `json:"search_no_index_enabled"`
-	NavigationContent                *topicModel.Navigation `json:"navigation_content"`
+	Count                            int              `json:"count"`
+	Type                             string           `json:"type"`
+	DatasetId                        string           `json:"dataset_id"`
+	DatasetTitle                     string           `json:"dataset_title"`
+	URI                              string           `json:"uri"`
+	Taxonomy                         []TaxonomyNode   `json:"taxonomy"`
+	Breadcrumb                       []TaxonomyNode   `json:"breadcrumb"`
+	IsInFilterBreadcrumb             bool             `json:"is_in_filter_breadcrumb"`
+	ServiceMessage                   string           `json:"service_message"`
+	Metadata                         Metadata         `json:"metadata"`
+	SearchDisabled                   bool             `json:"search_disabled"`
+	SiteDomain                       string           `json:"-"`
+	PatternLibraryAssetsPath         string           `json:"-"`
+	Language                         string           `json:"language"`
+	IncludeAssetsIntegrityAttributes bool             `json:"-"`
+	ReleaseDate                      string           `json:"release_date"`
+	BetaBannerEnabled                bool             `json:"beta_banner_enabled"`
+	CookiesPreferencesSet            bool             `json:"cookies_preferences_set"`
+	CookiesPolicy                    CookiesPolicy    `json:"cookies_policy"`
+	HasJSONLD                        bool             `json:"has_jsonld"`
+	FeatureFlags                     FeatureFlags     `json:"feature_flags"`
+	Error                            Error            `json:"error"`
+	EmergencyBanner                  EmergencyBanner  `json:"emergency_banner"`
+	Collapsible                      Collapsible      `json:"collapsible"`
+	Pagination                       Pagination       `json:"pagination"`
+	TableOfContents                  TableOfContents  `json:"table_of_contents"`
+	BackTo                           BackTo           `json:"back_to"`
+	SearchNoIndexEnabled             bool             `json:"search_no_index_enabled"`
+	NavigationContent                []NavigationItem `json:"navigation_content"`
+}
+
+// NavigationItem contains all information needed to render the navigation bar and submenus
+type NavigationItem struct {
+	Uri      string           `json:"uri"`
+	Label    string           `json:"label"`
+	SubItems []NavigationItem `json:"sub_item"`
 }
 
 // FeatureFlags contains toggles for certain features on the website
