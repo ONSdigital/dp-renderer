a59fa2e9af8fc1232ba227ae80afd2f9dee6899a
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2024-03-01 16:56:58 +0000

    2023 add model props to display input field validation error

--- model/input_date.go
+++ model/input_date.go
@@ -1,6 +1,7 @@
 package model
 
-/* InputDate provides an input for a date.
+/*
+InputDate provides an input for a date.
 Language should be passed in from the Page model.
 Id is the id in the document.
 InputNameDay is the name submitted for the day in a form.
@@ -9,18 +10,24 @@ InputNameYear is the name submitted for the year in a form.
 InputValueDay is the user input submitted for the day in a form.
 InputValueMonth is the user input submitted for the month in a form.
 InputValueYear is the user input submitted for the year in a form.
+HasDayValidationErr displays a validation error for the day input field.
+HasMonthValidationErr displays a validation error for the month input field.
+HasYearValidationErr displays a validation error for the year input field.
 Title states the purpose of the date.
 Description offers a further explanation for the purpose of the date.
 */
 type InputDate struct {
-	Language        string       `json:"language"`
-	Id              string       `json:"id"`
-	InputNameDay    string       `json:"input_name_day"`
-	InputNameMonth  string       `json:"input_name_month"`
-	InputNameYear   string       `json:"input_name_year"`
-	InputValueDay   string       `json:"input_value_day"`
-	InputValueMonth string       `json:"input_value_month"`
-	InputValueYear  string       `json:"input_value_year"`
-	Title           Localisation `json:"title"`
-	Description     Localisation `json:"description"`
+	Language              string       `json:"language"`
+	Id                    string       `json:"id"`
+	InputNameDay          string       `json:"input_name_day"`
+	InputNameMonth        string       `json:"input_name_month"`
+	InputNameYear         string       `json:"input_name_year"`
+	InputValueDay         string       `json:"input_value_day"`
+	InputValueMonth       string       `json:"input_value_month"`
+	InputValueYear        string       `json:"input_value_year"`
+	HasDayValidationErr   bool         `json:"has_day_validation_err"`
+	HasMonthValidationErr bool         `json:"has_month_validation_err"`
+	HasYearValidationErr  bool         `json:"has_year_validation_err"`
+	Title                 Localisation `json:"title"`
+	Description           Localisation `json:"description"`
 }
